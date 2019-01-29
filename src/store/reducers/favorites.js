b42b223918745f12570a8b5b3bd76716f7fb3402
banay.mario@gmail.com <banay.mario@gmail.com>
2019-01-29 09:56:22 +0100

    favorites.js cleaned

--- src/store/reducers/favorites.js
+++ src/store/reducers/favorites.js
@@ -2,7 +2,6 @@ import React from 'react';
 import * as actionTypes from '../actions';
 import beers from '../../assets/beers.json';
 import IcoMoon from 'react-icomoon';
-import { compose } from 'redux';
 
 const initialState = {
     beerData: beers,
@@ -10,49 +9,47 @@ const initialState = {
     cartData: [],
 }
 
-const star = {
+const cross = {
     width: '16px',
     height: '16px'
 };
 
-let crossIcon = <IcoMoon icon="cross" color="grey" style={star} />
-
+let crossIcon = <IcoMoon icon="cross" color="grey" style={cross} />
 
 const favoritesReducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.ADD_TO_FAVORITES:
-            let newArray = state.beerData.filter((el) => el.id === action.resultElId);
+            let itemToAddToFavorites = state.beerData.filter((el) => el.id === action.resultElId);
             return {
                 ...state,
                 favData: state.favData.concat(
                     {
-                        id: newArray[0].id,
-                        image: newArray[0].image_url,
-                        name: newArray[0].name,
-                        description: newArray[0].description,
+                        id: itemToAddToFavorites[0].id,
+                        image: itemToAddToFavorites[0].image_url,
+                        name: itemToAddToFavorites[0].name,
+                        description: itemToAddToFavorites[0].description,
                         button: crossIcon
                     })
             }
 
         case actionTypes.REMOVE_FROM_FAVORITES:
-            let newArray1 = state.favData.filter((el) => el.id !== action.resultElId);
-            //console.log(newArray1[0].id);
+            let itemToRemoveFromFavorites = state.favData.filter((el) => el.id !== action.resultElId);
             return {
                 ...state,
-                favData: newArray1
+                favData: itemToRemoveFromFavorites
             }
 
         case actionTypes.ADD_TO_CART:
-            let index = state.cartData.findIndex(item => item.id === action.resultElId);
-            let itemToAdd = state.beerData.filter((el) => el.id === action.resultElId);
-            if (index > -1) {
+            let indexOfElementToAdd = state.cartData.findIndex(item => item.id === action.resultElId);
+            let itemToAddToCart = state.beerData.filter((el) => el.id === action.resultElId);
+            if (indexOfElementToAdd > -1) {
                 let baseCart = state.cartData.filter((el) => el.id !== action.resultElId);
-                baseCart.splice(index, 0,
+                baseCart.splice(indexOfElementToAdd, 0,
                     {
-                        id: itemToAdd[0].id,
-                        image: itemToAdd[0].image_url,
-                        name: itemToAdd[0].name,
-                        description: itemToAdd[0].description,
+                        id: itemToAddToCart[0].id,
+                        image: itemToAddToCart[0].image_url,
+                        name: itemToAddToCart[0].name,
+                        description: itemToAddToCart[0].description,
                         quantity: 1,
                         button: crossIcon
                     });
@@ -62,53 +59,49 @@ const favoritesReducer = (state = initialState, action) => {
                     cartData: baseCart
                 }
             }
+
             else {
-                console.log("first time in cart");
                 return {
                     ...state,
                     cartData: state.cartData.concat(
                         {
-                            id: itemToAdd[0].id,
-                            image: itemToAdd[0].image_url,
-                            name: itemToAdd[0].name,
-                            description: itemToAdd[0].description,
+                            id: itemToAddToCart[0].id,
+                            image: itemToAddToCart[0].image_url,
+                            name: itemToAddToCart[0].name,
+                            description: itemToAddToCart[0].description,
                             quantity: 1,
                             button: crossIcon
                         })
                 }
             }
 
-
         case actionTypes.REMOVE_FROM_CART:
-            let newArray3 = state.cartData.filter((el) => el.id !== action.resultElId);
-            //console.log(newArray1[0].id);
+            let itemToRemoveFromToCart = state.cartData.filter((el) => el.id !== action.resultElId);
             return {
                 ...state,
-                cartData: newArray3
+                cartData: itemToRemoveFromToCart
             }
 
         case actionTypes.CHANGE_QUANTITY:
-            console.log("u reduceru");
-            let indexOfEl = state.cartData.findIndex(item => item.id === action.resultElId);
-            let itemToChange = state.beerData.filter((el) => el.id === action.resultElId);
-            if (indexOfEl > -1) {
+            let indexOfElementToChange = state.cartData.findIndex(item => item.id === action.resultElId);
+            let itemToChangeQuantity = state.beerData.filter((el) => el.id === action.resultElId);
+            if (indexOfElementToChange > -1) {
                 let baseCart = state.cartData.filter((el) => el.id !== action.resultElId);
-                baseCart.splice(indexOfEl, 0,
+                baseCart.splice(indexOfElementToChange, 0,
                     {
-                        id: itemToChange[0].id,
-                        image: itemToChange[0].image_url,
-                        name: itemToChange[0].name,
-                        description: itemToChange[0].description,
+                        id: itemToChangeQuantity[0].id,
+                        image: itemToChangeQuantity[0].image_url,
+                        name: itemToChangeQuantity[0].name,
+                        description: itemToChangeQuantity[0].description,
                         quantity: action.quantity,
                         button: crossIcon
                     });
-
                 return {
                     ...state,
                     cartData: baseCart
                 }
             }
-
+            
         default:
             return state;
     }
