236f5af6ba5d2751edd3a9b62d24863a4a62d1cb
Mario <mario-imac@Marios-iMac.local>
2020-07-15 19:31:00 +0200

    Updates

---
+++ precache-manifest.78140626644354a6fec73de25f349096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c74063683ba5debeb4be",
+    "url": "/beershop/static/css/main.5fa2277b.chunk.css"
+  },
+  {
+    "revision": "c74063683ba5debeb4be",
+    "url": "/beershop/static/js/main.c7406368.chunk.js"
+  },
+  {
+    "revision": "15c0f3f2f612c692a0e1",
+    "url": "/beershop/static/js/1.15c0f3f2.chunk.js"
+  },
+  {
+    "revision": "ce69d0aad4844046b006",
+    "url": "/beershop/static/js/runtime~main.ce69d0aa.js"
+  },
+  {
+    "revision": "d859c012e758dff9b8ab8542872969b6",
+    "url": "/beershop/static/media/duff.d859c012.png"
+  },
+  {
+    "revision": "18986f620217cba9d9f1977e8b26629c",
+    "url": "/beershop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/beershop-nks/precache-manifest.ed37257ec436b0f750cdaee230c4dbeb.js"
+  "/beershop/precache-manifest.78140626644354a6fec73de25f349096.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/beershop-nks/index.html", {
+workbox.routing.registerNavigationRoute("/beershop/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
